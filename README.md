3ca4834c75279c24c51148bfe2a6168b75f380ea
EvgenyDavydenko <frontend82@mail.ru>
2023-12-04 14:12:38 +0300

    simple Vue SPA skeleton

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1.  initial setup
 2.  install the frontend scaffolding using the laravel/ui
+3.  simple Vue SPA skeleton with vue-router
---
+++ resources/js/routes.js
@@ -0,0 +1,25 @@
+import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound';
+
+export default {
+    mode: 'history',
+    routes: [
+        {
+            path: '*',
+            component: NotFound
+
+        },
+        {
+            path: '/',
+            component: Home
+
+        },
+        {
+            path: '/about',
+            component: About
+
+        },
+    ]
+
+}
